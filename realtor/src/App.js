cdb9546b6e0b854f7d22268ca0482aa87af57698
eyob84 <eyobabebe28@gmail.com>
2025-07-14 23:29:33 +0300

    configuration

---
+++ realtor/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-2xl bg-red-400'>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
