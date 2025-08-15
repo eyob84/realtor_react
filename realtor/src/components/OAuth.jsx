b855a6dcb3bbd64ecfdeed693c45299bfc556c0c
eyob84 <eyobabebe28@gmail.com>
2025-08-15 16:31:54 +0300

    complete the Oauth functionality

--- realtor/src/components/OAuth.jsx
+++ realtor/src/components/OAuth.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { ref, get, child, set, serverTimestamp } from "firebase/database";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Create a reference to the user in Realtime DB
+      const dbRef = ref(db);
+      const snapshot = await get(child(dbRef, `users/${user.uid}`));
+
+      // If user does not exist, create them
+      if (!snapshot.exists()) {
+        await set(ref(db, "users/" + user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      toast.error("Could not authorize with Google");
+    }
+  }
+
   return (
-    <button className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded'><FcGoogle className='text-2xl bg-white rounded-full mr-2'/> Continue with Google</button>
-  )
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
+      <FcGoogle className="text-2xl bg-white rounded-full mr-2" /> Continue with Google
+    </button>
+  );
 }
